449bf1ae51a3ace3991d1e8ab7ba677b4880199e
Quix <quixcodes@gmail.com>
2023-09-23 03:50:05 +0200

    add app router clientside helper

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from "@/(components)/ui/skeleton";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <main className="h-full flex flex-col items-center">
+      <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+      Processing...
+    </main>
+  );
+}
