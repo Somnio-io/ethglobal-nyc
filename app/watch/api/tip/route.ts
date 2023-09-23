0bd735a3094fdbcf0f4fb9a09984a65644bd1914
Cedric <cedric@somnio.io>
2023-09-23 10:29:04 -0400

    Tipping api

--- app/watch/api/tip/route.ts
+++ app/watch/api/tip/route.ts
@@ -1,13 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-  let response: any[] = [];
-
-  return NextResponse.json({ response });
-}
-
 export async function POST(request: NextRequest) {
-  let response: any[] = [];
+  const response: any[] = [];
+
+  if (process.env.FEATURE_ENABLE_GASLESS_TRANSACTIONS) {
+    const { signedTransaction } = await request.json();
+
+    if (signedTransaction) {
+      try {
+        const apiResponse = await fetch(process.env.FEATURE_ENABLE_BICONOMY_PAYMASTER_URL as string, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": process.env.FEATURE_ENABLE_BICONOMY_API_KEY as string,
+          },
+          body: JSON.stringify({
+            transaction: signedTransaction,
+          }),
+        });
+
+        // Add the Biconomy response to your existing response
+        const apiData = await apiResponse.json();
+        console.log(apiData);
+        response.push({
+          biconomyResponse: apiData.data,
+        });
+      } catch (error) {
+        console.error("Error sponsoring transaction:", error);
+        response.push({
+          error: "Failed to sponsor transaction",
+        });
+      }
+    } else {
+      response.push({
+        error: "Signed transaction is not provided",
+      });
+    }
+  }
 
   return NextResponse.json({ response });
 }
