b371a2e3fd58863155c38f70673165668dde1267
Cedric <cedric@somnio.io>
2023-09-23 08:01:58 -0400

    Stream fetch route

--- app/watch/api/stream/route.ts
+++ app/watch/api/stream/route.ts
@@ -1,13 +1,28 @@
+import { LIVE_URL } from "@/(lib)/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   let response: any[] = [];
 
+  const getUrls = await fetch(`${LIVE_URL}`, {
+    method: "GET",
+    headers: {},
+  });
+  response = JSON.parse(await getUrls.json());
   return NextResponse.json({ response });
 }
 
 export async function POST(request: NextRequest) {
   let response: any[] = [];
+  const token = request.nextUrl.searchParams.get("token") as string;
+
+  const getUrls = await fetch(`${LIVE_URL}`, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+  });
+  response = JSON.parse(await getUrls.json());
 
   return NextResponse.json({ response });
 }
